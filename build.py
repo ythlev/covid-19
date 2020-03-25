6ae6c78bf7773051ca0d20f71c91e70e4b89f1c0
ythlev <60928242+ythlev@users.noreply.github.com>
2020-03-25 22:04:02 +0800

    new-york

--- build.py
+++ build.py
@@ -1,14 +1,14 @@
 # Created by Chang Chia-huan
 import argparse, pathlib, json, csv, io, urllib.request, math, statistics, datetime
 
-parser = argparse.ArgumentParser(description = "This script generates an svg maps for the COVID-19 outbreak for select countries")
-parser.add_argument("-c", "--country", help = "Name of country to generate; by default, a map for each country is generated")
+parser = argparse.ArgumentParser(description = "This script generates an svg maps for the COVID-19 outbreak for select places")
+parser.add_argument("-p", "--place", help = "Name of place to generate; by default, a map for each place is generated")
 args = vars(parser.parse_args())
 
-if args["country"] != None:
-    countries = [args["country"]]
+if args["place"] != None:
+    places = [args["place"]]
 else:
-    countries = ["Australia", "Canada", "France", "Germany", "Berlin", "Italy", "Japan", "Netherlands", "UK", "US", "Spain", "Taiwan"]
+    places = ["Australia", "Canada", "France", "Germany", "Berlin", "Italy", "Japan", "Netherlands", "UK", "US", "Spain", "Taiwan"]
 
 with open((pathlib.Path() / "population").with_suffix(".json"), newline = "", encoding = "utf-8-sig") as file:
     population = json.loads(file.read())
@@ -18,29 +18,29 @@ with open((pathlib.Path() / "meta").with_suffix(".json"), newline = "", encoding
 
 colour = ["#fee5d9","#fcbba1","#fc9272","#fb6a4a","#de2d26","#a50f15"]
 
-for country in countries:
+for place in places:
     main = {}
-    for place in population[country]:
-        main[place] = {
-            "population": population[country][place],
+    for area in population[place]:
+        main[area] = {
+            "population": population[place][area],
             "cases": 0
         }
     date = datetime.date.today()
-    if country == "Taiwan":
+    if place == "Taiwan":
         with urllib.request.urlopen("https://od.cdc.gov.tw/eic/Weekly_Age_County_Gender_19CoV.json") as response:
             cases = json.loads(response.read())
         for row in cases:
             main[row["縣市"]]["cases"] += int(row["確定病例數"])
-    elif country == "Malaysia":
+    elif place == "Malaysia":
         with open((pathlib.Path() / "cases").with_suffix(".json"), newline = "", encoding = "utf-8") as file:
             cases = json.loads(file.read())
-        for place in cases[country]:
-            main[place]["cases"] = cases[country][place]
+        for area in cases[place]:
+            main[area]["cases"] = cases[place][area]
     else:
-        if country == "UK":
+        if place == "UK":
             queries = ["UK-1", "UK-2"]
         else:
-            queries = [country]
+            queries = [place]
         for query in queries:
             url = "https://services{}.arcgis.com/{}/arcgis/rest/services/{}/FeatureServer/{}/".format(
                 meta[query][0][0],
@@ -54,26 +54,26 @@ for country in countries:
                 date = datetime.datetime.fromtimestamp(date, tz = datetime.timezone.utc)
             url2 = url + "query?where={}%3E0&outFields=*&returnGeometry=false&f=pjson".format(meta[query][0][4])
             with urllib.request.urlopen(url2) as response:
-                if country == "US":
+                if place == "US":
                     start = 236
                 else:
                     start = 0
                 for entry in json.loads(response.read())["features"][start:]:
                     if entry["attributes"][meta[query][1][0]] in main:
-                        if country == "Japan" and entry["attributes"][meta[query][1][0]] != "Unknown":
+                        if place == "Japan" and entry["attributes"][meta[query][1][0]] != "Unknown":
                             main[entry["attributes"][meta[query][1][0]]]["cases"] += 1
                         else:
                             main[entry["attributes"][meta[query][1][0]]]["cases"] = int(entry["attributes"][meta[query][1][1]])
 
-    if country in ["Germany", "Netherlands"]:
+    if place in ["Germany", "Netherlands", "New York"]:
         unit = 10000
     else:
         unit = 1000000
 
     values = []
-    for place in main:
-        main[place]["pcapita"] = main[place]["cases"] / main[place]["population"] * unit
-        values.append(main[place]["pcapita"])
+    for area in main:
+        main[area]["pcapita"] = main[area]["cases"] / main[area]["population"] * unit
+        values.append(main[area]["pcapita"])
 
     q = statistics.quantiles(values, n = 100, method = "inclusive")
     step = math.sqrt(statistics.mean(values) - q[1]) / 3
@@ -82,8 +82,8 @@ for country in countries:
     for i in range(1, 6):
         threshold[i] = math.pow(i * step, 2) + q[1]
 
-    with open((pathlib.Path() / "template" / country).with_suffix(".svg"), "r", newline = "", encoding = "utf-8") as file_in:
-        with open((pathlib.Path() / "results" / country).with_suffix(".svg"), "w", newline = "", encoding = "utf-8") as file_out:
+    with open((pathlib.Path() / "template" / place).with_suffix(".svg"), "r", newline = "", encoding = "utf-8") as file_in:
+        with open((pathlib.Path() / "results" / place).with_suffix(".svg"), "w", newline = "", encoding = "utf-8") as file_out:
             if threshold[1] > 10:
                 num = "{:.0f}"
             else:
@@ -91,15 +91,15 @@ for country in countries:
 
             for row in file_in:
                 written = False
-                for place in main:
-                    if row.find('id="{}"'.format(place)) > -1:
+                for area in main:
+                    if row.find('id="{}"'.format(area)) > -1:
                         i = 0
                         while i < 5:
-                            if main[place]["pcapita"] >= threshold[i + 1]:
+                            if main[area]["pcapita"] >= threshold[i + 1]:
                                 i += 1
                             else:
                                 break
-                        file_out.write(row.replace('id="{}"'.format(place), 'style="fill:{}"'.format(colour[i])))
+                        file_out.write(row.replace('id="{}"'.format(area), 'style="fill:{}"'.format(colour[i])))
                         written = True
                         break
 
@@ -117,6 +117,6 @@ for country in countries:
                         file_out.write(row)
 
     cases = []
-    for place in main:
-        cases.append(main[place]["cases"])
-    print("{}: {} cases total in {} areas".format(country, sum(cases), len(cases)))
+    for area in main:
+        cases.append(main[area]["cases"])
+    print("{}: {} cases total in {} areas".format(place, sum(cases), len(cases)))
